24bcab4405df69dc97b1e83bec70d3d795e8ba83
jakobkukla <jakob.kukla@gmail.com>
2018-07-03 00:02:19 +0200

    Added basic functioning

--- app/src/main/java/io/github/jakobkukla/openloadsf/MainActivity.kt
+++ app/src/main/java/io/github/jakobkukla/openloadsf/MainActivity.kt
@@ -1,12 +1,26 @@
 package io.github.jakobkukla.openloadsf
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_LINK = "io.github.openloadsf.LINK"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun fetchLink(view: View) {
+        val editText = findViewById<EditText>(R.id.editText1)
+        val link = editText.text.toString()
+        val intent = Intent(this, FetchStreamActivity::class.java).apply {
+            putExtra(EXTRA_LINK, link)
+        }
+        startActivity(intent)
     }
 }
